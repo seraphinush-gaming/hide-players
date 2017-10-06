18dadc428f4b7d31a81fbd821f9cd4a1c8985446
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-10-06 12:06:46 +0900

    Update index.js
    - Personalized code aesthetics
    - Removed Slash support
    - Removed String support
    - Added Pinkie Pie's Command module support

--- index.js
+++ index.js
@@ -1,63 +1,38 @@
-const S = require('./string')
+// OPCODE REQUIRED :
+// - C_SET_VISIBLE_RANGE
+// - S_SPAWN_USER
 
 module.exports = function HidePlayers(dispatch) {
-  let hidden = false
-  let visibleRange = 2500
 
-  dispatch.hook('C_WHISPER', 1, chatHook)
-  dispatch.hook('C_CHAT', 1, chatHook)
-  dispatch.hook('C_SET_VISIBLE_RANGE', 1, event => visibleRange = event.range)
-  dispatch.hook('S_SPAWN_USER', 1, () => { if (hidden) return false })
+	let enable = true
+	let visibleRange = 2500
 
-  function chatHook(event) {
-    const args = S.decodeHTMLEntities(S.stripTags(event.message))
-      .split(/\s+/)
-    const arg = args.reduce((out, part) => {
-      if (part.toLowerCase() === '!hide') return true
-      if (out === true) return part
-      return out
-    }, false)
+	// command
+	try {
+		const Command = require('command')
+		const command = Command(dispatch)
+		command.add('hide', () => {
+			enable = !enable
+			refreshNearbyPlayers()
+			send(`[hide-players] : Hide players ${enable ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'}.`)
+		})
+		function send(msg) {
+			command.message(`<font color="#FFFFFF">` + msg + `</font>`)
+		}
+	} catch (e) {
+		console.log(`[ERROR] -- hide-players module --`)
+	}
 
-    if (arg) {
-      evaluateArg(arg)
-      return false
-    }
-  }
+	// code
+	dispatch.hook('C_SET_VISIBLE_RANGE', 1, event => { visibleRange = event.range })
+	
+	dispatch.hook('S_SPAWN_USER', 1, () => { if (enable) return false })
 
-  function evaluateArg(arg) {
-    switch (arg) {
-      case 'on':
-        hidden = true
-        break
-      case 'off':
-        hidden = false
-        break
-      default:
-        hidden = !hidden
-    }
-
-    refreshNearbyPlayers()
-  }
-
-  function refreshNearbyPlayers() {
-    dispatch.toServer('C_SET_VISIBLE_RANGE', 1, { range: 1 })
-    setTimeout(() => {
-      dispatch.toServer('C_SET_VISIBLE_RANGE', 1, { range: visibleRange })
-    }, 1000)
-  }
-
-  // debug
-  // refreshNearbyPlayers()
-
-  // slash support
-  try {
-    const Slash = require('slash')
-    const slash = new Slash(dispatch)
-    slash.on('hide', args => {
-      evaluateArg(args[1])
-      slash.print('[hide-players] ' + (hidden ? 'enabled' : 'disabled'))
-    })
-  } catch (e) {
-    // do nothing because slash is optional
-  }
+	function refreshNearbyPlayers() {
+		dispatch.toServer('C_SET_VISIBLE_RANGE', 1, { range: 1 })
+		setTimeout(() => {
+			dispatch.toServer('C_SET_VISIBLE_RANGE', 1, { range: visibleRange })
+		}, 1000)
+	}
+	
 }

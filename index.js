4af30eee512b1e0223d8f049eb63b00c4af88d52
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-02-02 00:07:15 +0900

    2.04 r:00

--- index.js
+++ index.js
@@ -2,16 +2,27 @@
 // - C_SET_VISIBLE_RANGE
 // - S_GET_USER_LIST
 // - S_LEAVE_PARTY
+// - S_LOAD_TOPO
 // - S_LOGIN
 // - S_PARTY_MEMBER_LIST
 // - S_SPAWN_USER
 
-// Version 2.03 r:00
+// Version 2.04 r:00
+
+const blockZone = [
+    110,    // Battleground : Underground Arena
+    //7011,   // Guardian Legion mission : Shadow of the Gutrends
+    9720,   // Antaros' Abyss -- certain mechanic causes client to crash
+    9739,   // Rebel's Hideout -- certain mechanic causes client to crash
+    9920,   // Antaros' Abyss -- certain mechanic causes client to crash
+    9939,   // Rebel's Hideout (hard) -- certain mechanic causes client to crash
+]
 
 module.exports = function HidePlayers(d) {
 
     let enable = true,
         gameId = 0,
+        myZone = 0,
         visibleRange = 0
 
     let guild = [],
@@ -19,27 +30,38 @@ module.exports = function HidePlayers(d) {
 
     // code
     d.hook('C_SET_VISIBLE_RANGE', (e) => { visibleRange = e.range })
-    d.hook('S_LEAVE_PARTY', () => { party.length = 0 })
+    // refresh upon leaving party
+    d.hook('S_LEAVE_PARTY', () => { party.length = 0; refresh() })
+    d.hook('S_LOAD_TOPO', (e) => { myZone = e.zone })
     d.hook('S_LOGIN', (e) => { gameId = e.gameId })
 
+    // pre-req to load in guild members; definition 12 for K TERA
     d.hookOnce('S_GET_USER_LIST', 12, (e) => {
         for (let character of e.characters)
             if (!guild.includes(character.guild) && character.guild !== '')
                 guild.push(character.guild)
     })
 
+    // pre-req to load in party members
     d.hook('S_PARTY_MEMBER_LIST', (e) => {
+        if (blockZone.includes(myZone)) return
+        if (party.length == 0) refresh()
         for (let member of e.members)
             if (!member.gameId.equals(gameId))
                 party.push(member.gameId.toString())
     })
 
     d.hook('S_SPAWN_USER', (e) => {
+        if (blockZone.includes(myZone)) return
         if (enable && !(guild.includes(e.guild) || party.includes(e.gameId.toString()))) return false
     })
 
+    // credit : HugeDong69 for Guardian Legion mission crash fix
+    d.hook('S_FEARMOVE_STAGE', () => { if (enable) return false })
+    d.hook('S_FEARMOVE_END', () => { if (enable) return false })
+
     // helper
-    function refreshNearbyPlayers() {
+    function refresh() {
         d.toServer('C_SET_VISIBLE_RANGE', { range: 1 })
         setTimeout(() => {
             d.toServer('C_SET_VISIBLE_RANGE', { range: visibleRange })
@@ -54,11 +76,11 @@ module.exports = function HidePlayers(d) {
             // toggle
             if (!arg) {
                 enable = !enable
-                refreshNearbyPlayers()
+                refresh()
                 send(`Hide players ${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
             // refresh
             } else if (arg === 'refresh') {
-                refreshNearbyPlayers()
+                refresh()
                 send(`Refreshed.`.clr('56B4E9'))
             } else send(`Invalid argument.`.clr('FF0000'))
         })

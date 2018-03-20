956225200e6465f7176bda2422ae121cc9e4a957
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:33:36 +0900

    2.06 r:00

--- index.js
+++ index.js
@@ -1,104 +1,92 @@
-// OPCODE REQUIRED :
-// - C_SET_VISIBLE_RANGE
-// - S_GET_USER_LIST
-// - S_LEAVE_PARTY
-// - S_LOAD_TOPO
-// - S_LOGIN
-// - S_PARTY_MEMBER_LIST
-// - S_SPAWN_USER
+// Version 2.06 r:00
 
-// Version 2.05 r:01
+const Command = require('command')
+const config = require('./config.json')
+const instance = require('./instance.js')
 
-const instance = [
-	102, 103, 110, 111, 112, 116, 117, 118, 119, // Battlegrounds
-    //7011,   // Guardian Legion mission : Shadow of the Gutrends -- fixed
-    9710,   // Broken Prison -- certain mechanic causes client to crash
-    9720,   // Antaros' Abyss -- certain mechanic causes client to crash
-    9739,   // Rebel's Hideout -- certain mechanic causes client to crash
-    9920,   // Antaros' Abyss -- certain mechanic causes client to crash
-    9939,   // Rebel's Hideout (hard) -- certain mechanic causes client to crash
-]
+// credit : https://github.com/Some-AV-Popo
+String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
 
 module.exports = function HidePlayers(d) {
+    const command = Command(d)
 
-    let enable = true,
-        enableParty = true,
-        gameId = 0,
-        myZone = 0,
-        visibleRange = 0
+    let enable = config.enable,
+        enableParty = config.enableParty
 
     let guild = [],
-        party = []
+        myGameId = 0,
+        myZone = 0,
+        party = [],
+        visibleRange = 0
 
     // code
     d.hook('C_SET_VISIBLE_RANGE', (e) => { visibleRange = e.range })
-    // refresh upon leaving party
+    // reset existing array && refresh upon leaving party
     d.hook('S_LEAVE_PARTY', () => { party.length = 0; if (enable) refresh() })
     d.hook('S_LOAD_TOPO', (e) => { myZone = e.zone })
-    d.hook('S_LOGIN', (e) => { gameId = e.gameId })
+    d.hook('S_LOGIN', (e) => { myGameId = e.gameId })
+    
+    // credit : HugeDong69 for Guardian Legion mission crash fix
+    d.hook('S_FEARMOVE_STAGE', () => { if (enable) return false })
+    d.hook('S_FEARMOVE_END', () => { if (enable) return false })
 
-    // pre-req to load in guild members; definition 12 for K TERA
-    d.hookOnce('S_GET_USER_LIST', 12, (e) => {
+    // pre-req to load in guild members
+    d.hookOnce('S_GET_USER_LIST', (e) => {
         for (let character of e.characters)
-            if (!guild.includes(character.guild) && character.guild !== '')
-                guild.push(character.guild)
+            if (!guild.includes(character.guildId) && character.guildId !== 0)
+                guild.push(character.guildId)
     })
 
+    // TODO
     // pre-req to load in party members
+    // if new party refresh
+    // for every member in the party, ignore self
+    // then add new party members to the list
     d.hook('S_PARTY_MEMBER_LIST', (e) => {
         if (instance.includes(myZone)) return
-        if (enable && (party.length === 0 || party.length !== e.members.length)) refresh()
+        if (party.length == 0) refresh()
         for (let member of e.members)
-            if (!member.gameId.equals(gameId))
+            if (member.gameId.equals(myGameId)) continue
+            else if (!party.includes(member.gameId.toString()))
                 party.push(member.gameId.toString())
     })
 
+    // TODO
     d.hook('S_SPAWN_USER', (e) => {
         if (instance.includes(myZone)) return
         if (enable) {
             if (enableParty) {
-                if (!(guild.includes(e.guild) || party.includes(e.gameId.toString()))) return false
+                if (!(guild.includes(e.guildEmblemId) || party.includes(e.gameId.toString()))) return false
+                else return
             }
             else return false
         }
     })
 
-    // credit : HugeDong69 for Guardian Legion mission crash fix
-    d.hook('S_FEARMOVE_STAGE', () => { if (enable) return false })
-    d.hook('S_FEARMOVE_END', () => { if (enable) return false })
-
     // helper
     function refresh() {
         d.toServer('C_SET_VISIBLE_RANGE', { range: 1 })
-        setTimeout(() => {
-            d.toServer('C_SET_VISIBLE_RANGE', { range: visibleRange })
-        }, 1000)
+        setTimeout(() => { d.toServer('C_SET_VISIBLE_RANGE', { range: visibleRange }) }, 1000)
     }
 
     // command
-    try {
-        const Command = require('command')
-        const command = Command(d)
-        command.add(['hide', 'ㅗㅑㅇㄷ'], (arg) => {
-            // toggle
-            if (!arg) {
-                enable = !enable
-                refresh()
-                send(`Hide players ${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
-            // refresh
-            } else if (arg === 'a' || arg === 'ㅁ') {
-                enableParty = !enableParty
-                refresh()
-                send(`Show guild/party members ${enableParty ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
-            } else if (arg === 'r' || arg === 'ㄱ' || arg === 'refresh') {
-                refresh()
-                send(`Refreshed.`.clr('56B4E9'))
-            } else send(`Invalid argument.`.clr('FF0000'))
-        })
-        function send(msg) { command.message(`[hide-players] : ` + [...arguments].join('\n\t - ')) }
-    } catch (e) { console.log(`[ERROR] -- hide-players module --`) }
+    command.add(['hide', 'ㅗㅑㅇㄷ'], (arg) => {
+        // toggle
+        if (!arg) {
+            enable = !enable
+            refresh()
+            send(`${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
+        // hide/show all player
+        } else if (arg === 'a' || arg === 'ㅁ') {
+            enableParty = !enableParty
+            refresh()
+             send(`Show guild/party members ${enableParty ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}`)
+        // refresh
+        } else if (arg === 'r' || arg === 'ㄱ') {
+            refresh()
+            send(`Refreshed`.clr('56B4E9'))
+        } else send(`Invalid argument.`.clr('FF0000'))
+    })
+    function send(msg) { command.message(`[hide-players] : ` + msg) }
 
 }
-
-// credit : https://github.com/Some-AV-Popo
-String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }

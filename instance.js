12a07769d23e6594116b7df83327b55856f466eb
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:36:23 +0900

    2.06 auto-update support

---
+++ instance.js
@@ -0,0 +1,8 @@
+module.exports = [
+	102, 103, 110, 111, 112, 116, 117, 118, 119, // Battlegrounds
+    9710,   // Broken Prison -- certain mechanic causes client to crash
+    9720,   // Antaros' Abyss -- certain mechanic causes client to crash
+    9739,   // Rebel's Hideout -- certain mechanic causes client to crash
+    9920,   // Antaros' Abyss -- certain mechanic causes client to crash
+    9939,   // Rebel's Hideout (hard) -- certain mechanic causes client to crash
+]
